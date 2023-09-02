aa6e89be0f1fef92164080d7bc4199f06860ddc4
taulmax <taulmax@gmail.com>
2023-09-02 21:19:59 +0900

    [feat] 버거 메뉴 완료. 모바일 화면 해야함

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,16 @@ import styles from "@/styles/components/Header.module.css";
 import Nav from "./Nav";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faCircleInfo,
+  faGlobe,
+  faMagnifyingGlass,
+  faPaw,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import { useCallback, useState } from "react";
+import Button from "./Button";
 
 export default function Header() {
   const [isBurgerMenuOn, setIsBurgerMenuOn] = useState(false);
@@ -39,7 +47,35 @@ export default function Header() {
         className={`${styles.burger_navigation} ${
           isBurgerMenuOn ? styles.burger_on : styles.burger_off
         }`}
-      ></div>
+      >
+        <header className={styles.burger_header}>
+          <div className={styles.burger_logo}>PIT-A-PET</div>
+          <FontAwesomeIcon
+            onClick={onClickBurgerMenu}
+            className={styles.burger_close_button}
+            icon={faXmark}
+          />
+        </header>
+        <div className={`pl20 pr20 pb20 ${styles.login_wrapper}`}>
+          <Button color="logo" text="로그인" />
+        </div>
+        <li className={styles.burger_list}>
+          <FontAwesomeIcon icon={faPaw} />
+          <div>아이들 소개</div>
+        </li>
+        <li className={styles.burger_list}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <div>우리 아이 찾기</div>
+        </li>
+        <li className={styles.burger_list}>
+          <FontAwesomeIcon icon={faCircleInfo} />
+          <div>반려동물 관련정보</div>
+        </li>
+        <li className={styles.burger_list}>
+          <FontAwesomeIcon icon={faGlobe} />
+          <div>커뮤니티</div>
+        </li>
+      </div>
       <div
         onClick={onClickBurgerMenu}
         className={`${styles.burger_dark_layer} ${isBurgerMenuOn ? "" : "off"}`}
