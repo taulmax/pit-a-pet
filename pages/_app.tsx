a4e3d49ebb36ced35d22d223cd59b4786b770fc6
taulmax <taulmax@gmail.com>
2023-09-02 14:42:47 +0900

    [feat] 메인페이지일때와 아닐때로 _app 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import Header from "@/components/Header";
 import "@/styles/reset.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
   return (
     <>
       <Header />
-      <Component {...pageProps} />;
+      {pathname === "/" ? (
+        <Component {...pageProps} />
+      ) : (
+        <div className="app_wrapper">
+          <Component {...pageProps} />
+        </div>
+      )}
     </>
   );
 }
