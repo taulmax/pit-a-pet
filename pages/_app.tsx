bbad226c05bd9f350e4cbd2f4226a5343002f3ed
taulmax <taulmax@gmail.com>
2023-09-03 22:17:47 +0900

    [feat] favicon, title 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import Header from "@/components/Header";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
   return (
     <>
+      <Head>
+        <title>Pit a pet</title>
+        <link rel="icon" href="/img/logo.png" />
+      </Head>
       <Header />
       {pathname === "/" ? (
         <Component {...pageProps} />
