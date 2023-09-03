dd608668386d89ec785201083953237c8d36bc08
taulmax <taulmax@gmail.com>
2023-09-03 21:16:05 +0900

    [feat] 모바일 반응형 메인페이지 완료

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import {
   faBars,
   faCircleInfo,
   faGlobe,
+  faHouse,
   faMagnifyingGlass,
   faPaw,
   faXmark,
@@ -59,22 +60,36 @@ export default function Header() {
         <div className={`pl20 pr20 pb20 ${styles.login_wrapper}`}>
           <Button color="logo" text="로그인" />
         </div>
-        <li className={styles.burger_list}>
-          <FontAwesomeIcon icon={faPaw} />
-          <div>아이들 소개</div>
-        </li>
-        <li className={styles.burger_list}>
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-          <div>우리 아이 찾기</div>
-        </li>
-        <li className={styles.burger_list}>
-          <FontAwesomeIcon icon={faCircleInfo} />
-          <div>반려동물 관련정보</div>
-        </li>
-        <li className={styles.burger_list}>
-          <FontAwesomeIcon icon={faGlobe} />
-          <div>커뮤니티</div>
-        </li>
+        <Link href="/">
+          <li className={styles.burger_list}>
+            <FontAwesomeIcon icon={faHouse} />
+            <div>홈</div>
+          </li>
+        </Link>
+        <Link href="/introduction">
+          <li className={styles.burger_list}>
+            <FontAwesomeIcon icon={faPaw} />
+            <div>아이들 소개</div>
+          </li>
+        </Link>
+        <Link href="/find">
+          <li className={styles.burger_list}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <div>우리 아이 찾기</div>
+          </li>
+        </Link>
+        <Link href="/info">
+          <li className={styles.burger_list}>
+            <FontAwesomeIcon icon={faCircleInfo} />
+            <div>반려동물 관련정보</div>
+          </li>
+        </Link>
+        <Link href="/community">
+          <li className={styles.burger_list}>
+            <FontAwesomeIcon icon={faGlobe} />
+            <div>커뮤니티</div>
+          </li>
+        </Link>
       </div>
       <div
         onClick={onClickBurgerMenu}
