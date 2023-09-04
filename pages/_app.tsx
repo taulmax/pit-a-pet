ad76bc40f8c5b036cd2bdc43570d9b7419793632
taulmax <taulmax@gmail.com>
2023-09-04 23:25:59 +0900

    [feat] react query 준비

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,15 @@ import Header from "@/components/Header";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <title>Pit a pet</title>
         <link rel="icon" href="/img/logo.png" />
@@ -21,6 +25,6 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </div>
       )}
-    </>
+    </QueryClientProvider>
   );
 }
