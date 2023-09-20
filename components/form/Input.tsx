8b6338ab11110c75135f256278d03c084001a94e
taulmax <taulmax@gmail.com>
2023-09-20 16:59:29 +0900

    [feat] Input Component에 suffix 추가

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -15,6 +15,7 @@ interface IInput {
   className?: string;
   value: string;
   onChange: any;
+  suffix?: string;
 }
 
 export default function Input({
@@ -23,26 +24,30 @@ export default function Input({
   className = "",
   value,
   onChange,
+  suffix,
 }: IInput) {
   // Input Focus Event
   const onFocus = useCallback((e: React.FocusEvent<HTMLInputElement>) => {
-    e.target.classList.add(styles.active);
+    e.target.parentElement?.classList.add(styles.active);
   }, []);
 
   // Input Blur Event
   const onBlur = useCallback((e: React.FocusEvent<HTMLInputElement>) => {
-    e.target.classList.remove(styles.active);
+    e.target.parentElement?.classList.remove(styles.active);
   }, []);
 
   return (
-    <input
-      id={id}
-      className={`${styles.custom_input} ${className}`}
-      type={type}
-      value={value}
-      onChange={onChange}
-      onFocus={onFocus}
-      onBlur={onBlur}
-    />
+    <div className={styles.input_wrapper}>
+      <input
+        id={id}
+        className={`${styles.custom_input} ${className}`}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+      {suffix && <div className={styles.suffix}>{suffix}</div>}
+    </div>
   );
 }
