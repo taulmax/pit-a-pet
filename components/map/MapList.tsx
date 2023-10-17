a4cd954e26ed501a18268f959487fc0c7d6e360a
taulmax <taulmax@gmail.com>
2023-10-17 10:11:28 +0900

    [feat] 맵 리스트 작성중

--- components/map/MapList.tsx
+++ components/map/MapList.tsx
@@ -1,5 +1,18 @@
 import styles from "@/styles/components/map/MapList.module.css";
 
-export default function MapList() {
-  return <div className={styles.info_wrapper}></div>;
+interface IMapList {
+  tab: "동물병원" | "동물보호소" | "동물등록 대행업체";
+}
+
+export default function MapList({ tab }: IMapList) {
+  return (
+    <div className={styles.info_wrapper}>
+      <h1 className={styles.title}>
+        주변&nbsp;<span>{tab}</span>&nbsp;검색결과
+      </h1>
+      <ul className={styles.content_list}>
+        <li className={styles.content}></li>
+      </ul>
+    </div>
+  );
 }
--- pages/map.tsx
+++ pages/map.tsx
@@ -7,28 +7,41 @@ import {
   faStethoscope,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 export default function Map() {
+  const [tab, setTab] = useState<
+    "동물병원" | "동물보호소" | "동물등록 대행업체"
+  >("동물병원");
   return (
     <div className={styles.map_wrapper}>
       <div className={styles.naver_map_wrapper}>
         <NaverMap />
         <ul className={styles.floating_tab}>
-          <li>
+          <li
+            className={tab === "동물병원" ? styles.active : ""}
+            onClick={() => setTab("동물병원")}
+          >
             <FontAwesomeIcon icon={faStethoscope} color="green" />
             &nbsp; 동물병원
           </li>
-          <li>
+          <li
+            className={tab === "동물보호소" ? styles.active : ""}
+            onClick={() => setTab("동물보호소")}
+          >
             <FontAwesomeIcon icon={faHandHoldingHeart} color="red" />
             &nbsp; 동물보호소
           </li>
-          <li>
+          <li
+            className={tab === "동물등록 대행업체" ? styles.active : ""}
+            onClick={() => setTab("동물등록 대행업체")}
+          >
             <FontAwesomeIcon icon={faBuilding} color="grey" />
             &nbsp; 동물등록 대행업체
           </li>
         </ul>
       </div>
-      <MapList />
+      <MapList tab={tab} />
     </div>
   );
 }
