7796ef424572380d3fd2a5f25b64e631ddc37540
taulmax <taulmax@gmail.com>
2023-10-18 10:13:29 +0900

    [fix] 안쓰는 import 제거

--- pages/lost/[id].tsx
+++ pages/lost/[id].tsx
@@ -1,9 +1,9 @@
-import { decodeBase64ToUTF8, formatDate } from "@/util/util";
+import { formatDate } from "@/util/util";
 import { useRouter } from "next/router";
 import styles from "@/styles/pages/introductionDetail.module.css";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import { LostData, getLostDetail } from "@/api/lost";
+import { useEffect } from "react";
+import { getLostDetail } from "@/api/lost";
 import { useGlobalState } from "@/context/GlobalStateContext";
 import { useQuery } from "react-query";
 
