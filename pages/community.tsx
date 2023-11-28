90b1aac0ba40c9a0abaad5683186605e741d39dd
JinjangKang <wkdwlsrkd97@gmail.com>
2023-11-28 19:58:21 +0900

    [fix]커뮤니티 리스트 스크롤

--- pages/community.tsx
+++ pages/community.tsx
@@ -12,133 +12,143 @@ import { useQuery } from "react-query";
 
 // query 프롭스에 대한 타입 정의
 type CommunityProps = {
-  query: {
-    page?: string;
-    type?: "recent" | "hot";
-  };
+    query: {
+        page?: string;
+        type?: "recent" | "hot";
+    };
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { query } = context;
-  return {
-    props: {
-      query,
-    },
-  };
+    const { query } = context;
+    return {
+        props: {
+            query,
+        },
+    };
 };
 
 export default function Community({ query }: CommunityProps) {
-  const router = useRouter();
-  const currentPage = query.page ? Number(query.page) : 1;
-  const currentType = query.type ? query.type : "recent";
+    const router = useRouter();
+    const currentPage = query.page ? Number(query.page) : 1;
+    const currentType = query.type ? query.type : "recent";
 
-  const { isLogin } = useGlobalState();
-  const dialogRef = useRef<HTMLDialogElement>(null);
+    const { isLogin } = useGlobalState();
+    const dialogRef = useRef<HTMLDialogElement>(null);
 
-  const onClickStory = useCallback(
-    (id: number) => {
-      router.push(`/community/${id}`);
-    },
-    [router]
-  );
-
-  const onPageChange = useCallback(
-    (page: number) => {
-      router.push(`/community?page=${page}`);
-      const element = document.getElementById("community_main_wrapper");
-      if (element) {
-        element.scrollTop = 0;
-      }
-    },
-    [router]
-  );
-
-  const { data, isLoading, isError } = useQuery(["story", query], () =>
-    getStory({
-      page: currentPage,
-      pageSize: 20,
-    })
-  );
-
-  // 글쓰기 버튼 클릭
-  const onClickWriteButton = useCallback(() => {
-    if (isLogin) {
-      router.push("/communityWrite");
-    } else {
-      dialogRef.current?.showModal();
-    }
-  }, [isLogin, router]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error</div>;
-  }
-
-  if (!data?.data.length) {
-    return (
-      <main
-        id="community_main_wrapper"
-        className={styles.community_main_wrapper}
-      >
-        <div>No data available</div>
-
-        {/* 우리 아이 찾기 글쓰기 FIXED BUTTON */}
-        <div onClick={onClickWriteButton} className={styles.write_button}>
-          <FontAwesomeIcon icon={faPen} />
-        </div>
-      </main>
+    const onClickStory = useCallback(
+        (id: number) => {
+            router.push(`/community/${id}`);
+        },
+        [router]
     );
-  }
 
-  return (
-    <main id="community_main_wrapper" className={styles.community_main_wrapper}>
-      <header>
-        <div className={styles.community_recent_or_hot_tab}>
-          <div className={currentType === "recent" ? styles.active : ""}>
-            최신글
-          </div>
-          <div className={currentType === "hot" ? styles.active : ""}>
-            인기글
-          </div>
-        </div>
-      </header>
-      <ul>
-        <li className={styles.community_th}>
-          <div className={styles.number}>번호</div>
-          <div className={styles.title}>제목</div>
-          <div className={styles.author}>작성자</div>
-          <div className={styles.date}>날짜</div>
-          <div className={styles.visit}>조회</div>
-          <div className={styles.thumbsup}>추천</div>
-        </li>
-        {data.data.map((story) => (
-          <li
-            onClick={() => onClickStory(story.post_id)}
-            key={story.post_id}
-            className={styles.community_tr}
-          >
-            <div className={styles.number}>{story.post_id}</div>
-            <div className={styles.title}>{story.title}</div>
-            <div className={styles.author}>{story.username}</div>
-            <div className={styles.date}>
-              {formatTimeDifference(story.created_at)}
+    const onPageChange = useCallback(
+        (page: number) => {
+            router.push(`/community?page=${page}`);
+            const element = document.getElementById("community_main_wrapper");
+            if (element) {
+                element.scrollTop = 0;
+            }
+        },
+        [router]
+    );
+
+    const { data, isLoading, isError } = useQuery(["story", query], () =>
+        getStory({
+            page: currentPage,
+            pageSize: 20,
+        })
+    );
+
+    // 글쓰기 버튼 클릭
+    const onClickWriteButton = useCallback(() => {
+        if (isLogin) {
+            router.push("/communityWrite");
+        } else {
+            dialogRef.current?.showModal();
+        }
+    }, [isLogin, router]);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) {
+        return <div>Error</div>;
+    }
+
+    if (!data?.data.length) {
+        return (
+            <main
+                id="community_main_wrapper"
+                className={styles.community_main_wrapper}
+            >
+                <div>No data available</div>
+
+                {/* 우리 아이 찾기 글쓰기 FIXED BUTTON */}
+                <div
+                    onClick={onClickWriteButton}
+                    className={styles.write_button}
+                >
+                    <FontAwesomeIcon icon={faPen} />
+                </div>
+            </main>
+        );
+    }
+
+    return (
+        <main
+            id="community_main_wrapper"
+            className={styles.community_main_wrapper}
+        >
+            <header>
+                <div className={styles.community_recent_or_hot_tab}>
+                    <div
+                        className={
+                            currentType === "recent" ? styles.active : ""
+                        }
+                    >
+                        최신글
+                    </div>
+                    <div className={currentType === "hot" ? styles.active : ""}>
+                        인기글
+                    </div>
+                </div>
+            </header>
+            <ul>
+                <li className={styles.community_th}>
+                    <div className={styles.number}>번호</div>
+                    <div className={styles.title}>제목</div>
+                    <div className={styles.author}>작성자</div>
+                    <div className={styles.date}>날짜</div>
+                    <div className={styles.visit}>조회</div>
+                    <div className={styles.thumbsup}>추천</div>
+                </li>
+                {data.data.map((story) => (
+                    <li
+                        onClick={() => onClickStory(story.post_id)}
+                        key={story.post_id}
+                        className={styles.community_tr}
+                    >
+                        <div className={styles.number}>{story.post_id}</div>
+                        <div className={styles.title}>{story.title}</div>
+                        <div className={styles.author}>{story.username}</div>
+                        <div className={styles.date}>
+                            {formatTimeDifference(story.created_at)}
+                        </div>
+                        <div className={styles.visit}>39</div>
+                        <div className={styles.thumbsup}>8</div>
+                    </li>
+                ))}
+            </ul>
+
+            {/* 글쓰기 FIXED BUTTON */}
+            <div onClick={onClickWriteButton} className={styles.write_button}>
+                <FontAwesomeIcon icon={faPen} />
             </div>
-            <div className={styles.visit}>39</div>
-            <div className={styles.thumbsup}>8</div>
-          </li>
-        ))}
-      </ul>
-
-      {/* 글쓰기 FIXED BUTTON */}
-      <div onClick={onClickWriteButton} className={styles.write_button}>
-        <FontAwesomeIcon icon={faPen} />
-      </div>
-      <dialog ref={dialogRef}>
-        <LoginDialog dialogRef={dialogRef} />
-      </dialog>
-    </main>
-  );
+            <dialog ref={dialogRef}>
+                <LoginDialog dialogRef={dialogRef} />
+            </dialog>
+        </main>
+    );
 }
