73e8a7572acfc246177299f5a5582e30dd860a7f
taulmax <taulmax@gmail.com>
2023-12-06 08:08:16 +0900

    [feat] axios baseURL 핫스팟으로 되어있던거 해결

--- api/axios.ts
+++ api/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:8080",
-  baseURL: "http://192.168.165.59:8080",
+  baseURL: "http://localhost:8080",
+  // baseURL: "http://192.168.165.59:8080",
 });
 
 export default instance;
