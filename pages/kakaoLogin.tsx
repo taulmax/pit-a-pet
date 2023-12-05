def26573bc39dee25b02204cd2790ac8421159dd
taulmax <taulmax@gmail.com>
2023-12-06 03:47:50 +0900

    [feat] 카카오 로그인 끝

--- pages/kakaoLogin.tsx
+++ pages/kakaoLogin.tsx
@@ -1,22 +1,30 @@
-import { kakaoLoginCallback } from "@/api/login";
+import { kakaoLoginCallback, useLogin, useRegister } from "@/api/login";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function KakaoLogin() {
   const router = useRouter();
+  const { register } = useRegister();
+  const { loginWithCredentials } = useLogin();
 
   useEffect(() => {
     // 페이지 로딩 시 URL에서 'code' 파라미터를 가져와서 사용
     const { code } = router.query;
 
     if (code) {
-      // 여기에서 code를 사용하여 필요한 작업을 수행
-      console.log("Kakao Login Code:", code);
-
       // 예를 들어, code를 서버로 보내어 토큰을 교환하거나 다른 로직을 수행할 수 있습니다.
       kakaoLoginCallback(code as string)
         .then((response) => {
           console.log(response);
+          register({
+            username: response.username,
+            password: `${response.kakaoId}`,
+          }).then(() =>
+            loginWithCredentials({
+              username: response.username,
+              password: `${response.kakaoId}`,
+            })
+          );
         })
         .catch((error) =>
           console.error("Error exchanging code for token:", error)
